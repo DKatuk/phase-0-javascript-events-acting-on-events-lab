f08e9823a110dbe9fbdb66993b822b072ac57940
Dilara Katuk <dilarakatuk@gmail.com>
2022-06-02 17:54:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+
+document.addEventListener("keydown", function(event) {
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+    else if(event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+    else {
+        alert('Please use the left or right arrow keys to move the dodger');
+    }
+  })
+
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
